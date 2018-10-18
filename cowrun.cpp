83d84ca4f05e20a90a089ec6ad76c6fe375a2d7a
bongoStudios <hola@gmail.com>
2018-10-18 17:31:34 -0300

    added generation of cows and jockeys, and the choosing of them

--- cowrun.cpp
+++ cowrun.cpp
@@ -6,60 +6,74 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "random.hpp"
 #define WIDTH 30
 #define HEIGHT 10 
 
 using namespace std;
 
-int startx = 0;
-int starty = 0;
+int startx = 0, starty = 0, firstNamesLength = 59,
+lastNamesLength = 150, cowNamesLength = 15;
 
-class Cow {
-public:
-	Cow (char[24],int,int,int,int);
+
+int *throwDice(int cantidad, int dado)  {
+	int *dados = new int[cantidad];
+	for(int i = 0; i<cantidad; i++)
+		dados[i] = randomNo(dado, 1);
+	return dados;
+}
+
+struct Cow {
+	void declare(char naming[24]) {
+		for(int i = 0; i < 24; i++)
+			name[i] = naming[i];
+		intelligence = randomNo(5, -5);
+		dexterity = randomNo(5, -5);
+		strength = randomNo(5, -5);
+		willpower = randomNo(5, -5);
+	}
 	int intelligence, dexterity, strength, willpower;
 	char name[24];
 };
-Cow::Cow(char naming[24], int intel, int dext, int stren, int willp) {
-	for(int i = 0; i < 24; i++)
-		name[i] = naming[i];
-	intelligence = intel;
-	dexterity = dext;
-	strength = stren;
-	willpower = willp;
-}
 
-class Jockey {
-public:
-	Jockey (char[24], char[24], int, int);
+struct Jockey {
+	void declare(char nameJ[24], char surNameJ[24]) {
+		for(int i = 0; i < 24; i++) {
+			firstName[i] = nameJ[i];
+			lastName[i] = surNameJ[i];
+		}
+		riding = randomNo(5, -5);
+		luck = randomNo(5, -5);
+	}
 	int riding, luck;
 	char firstName[24], lastName[24];
+	Cow cow;
 };
-Jockey::Jockey(char nameJ[24], char surnameJ[24], int rideSkill, int luckStat) {
 
-}
 
 char firstNames[][24] = { "Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Rosaline", "Steve", "Thomas", "Tim", "Tyler", "Victor", "Walter", "Julian", "Rory", "Maximilian", "Angela", "Dmitri", "Vladimir", "Moriah", "Jebediah", "Viktor", "Jeff", "Timothy", "Jari", "Robin", "Alessandra", "Moe", "Duncan", "Layla", "Lia", "Daniel", "Beatrice", "Bjorn", "Sullivan", "Dorothy", "Joel", "Kenny", "Robert"},
 lastNames[][24] = { "Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler" },
 cowNames[][24] = { "Becky", "Bessy", "Abby", "Gertrudis", "Bassy", "Danielle", "Prima", "Yeeta", "Julia", "Adrianna", "Caecilia", "Balbina", "Leufelia", "Cornelia", "Lewa" };
 
 char *choices[10] = { 
-			"Rock",
-			"Paper",
-			"Scissors",
+			"Start",
 			"Exit",
 		  };
 		  
-int n_choices = 4;
+int n_choices = 2;
 
 void print_menu(WINDOW *menu_win, int highlight);
+void makeTheJockey(int &i, Jockey jockeys[5]);
+void printMenu(WINDOW *menu_win, int &choice);
+void cleanChoices() {
+	for(int i = 0; i < 10; i++)
+		choices[i] = "";
+}
 
 int main () {
     WINDOW *menu_win;
-    int highlight;
-	int choice;
-	int c;
+	int c, choice;
 
     initscr();
 	clear();
@@ -73,12 +87,99 @@ int main () {
 	keypad(menu_win, TRUE);
 
     while(1) {
-        choice = 0;
-		highlight = 1;
+        Jockey jockeys[5];
+		int i = 0;
 		clear();
-        printw("Calculating stats for new cow race");
+		mvprintw(0, 0, "Calculating stats for the new cow race");
+		refresh();
+		makeTheJockey(i, jockeys);
+		printw(".");
+		refresh();
+		makeTheJockey(i, jockeys);
+		printw(".");
+		refresh();
+		makeTheJockey(i, jockeys);	
+		printw(". ");
+		refresh();
+		makeTheJockey(i, jockeys);
+		makeTheJockey(i, jockeys);
+		printw("done\n");
+		refresh();
+		cleanChoices();
+		choices[0] = "Start", choices[1] = "Exit", n_choices = 2;
+		printMenu(menu_win, choice);
+		if(choice == 2) {
+			endwin();
+			return 0;
+		}
+		clear();
+		mvprintw(0, 0, "How fortunate, let's bet then. Who you choosin'?");
+		cleanChoices();
+		n_choices = 5;
+		for(int i = 0; i < n_choices; i++) {
+			Jockey current = jockeys[i];
+			char *str = new char[79];
+			strcpy(str, current.lastName);
+			strcat(str, " - ");
+			strcat(str, current.cow.name);
+			choices[i] = str;
+		}
+		printMenu(menu_win, choice);
+		c = 110;
+		while(true) {
+			clear();
+			Jockey current = jockeys[choice-1];
+			mvprintw(0, 0, "Jockey:\n\tFirst Name: %s\n\tLast Name: %s\n", current.firstName, current.lastName);
+			printw("Cow:\n\tName: %s\n", current.cow.name);
+			printw("Are you sure about this? (y/n):");
+			refresh();
+			c = getch();
+			if(c == 121 or c == 89)
+				break;
+			clear();
+			mvprintw(0, 0, "Oh ok... then, Who you choosin'?");
+			printMenu(menu_win, choice);
+		}
+		endwin();
+		return 0;
     }
     return 0;
+}
+
+void makeTheJockey(int &i, Jockey jockeys[5]) {
+	jockeys[i].declare(firstNames[randomNo(firstNamesLength - 1, 0)], lastNames[randomNo(lastNamesLength - 1, 0)]);
+	jockeys[i].cow.declare(cowNames[randomNo(cowNamesLength - 1, 0)]);
+	i++;
+}
+
+void printMenu(WINDOW *menu_win, int &choice) {
+	choice = 0;
+	int c, highlight = 1;
+	refresh();
+	print_menu(menu_win, highlight);
+	while(1) {	
+			c = wgetch(menu_win);
+			switch(c) {	
+				case 87: case 119: case KEY_UP:
+					if(highlight == 1)
+						highlight = n_choices;
+					else
+						--highlight;
+					break;
+				case 83: case 115: case KEY_DOWN:
+					if(highlight == n_choices)
+						highlight = 1;
+					else 
+						++highlight;
+					break;
+				case 10:
+					choice = highlight;
+					break;
+			}
+			print_menu(menu_win, highlight);
+			if(choice != 0)
+				break;
+		}
 }
 
 void print_menu(WINDOW *menu_win, int highlight)
